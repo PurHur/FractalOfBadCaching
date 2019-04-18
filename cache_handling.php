32eff188e3719cf2094edf76267f3cf384cb8f32
PurHur <purhur@purh.pw>
2019-04-18 17:24:11 +0200

    Write unix file standard cache. More standards

--- cache_handling.php
+++ cache_handling.php
@@ -3,7 +3,7 @@ $GLOBALS['cache'] = @include('cache.php');
 
 function saveCache()
 {
-    $cache = "<?php\nreturn unserialize('".serialize($GLOBALS['cache'])."');";
+    $cache = "<?php\nreturn unserialize('".serialize($GLOBALS['cache'])."');\n";
     file_put_contents('cache.php',$cache);
 }
 register_shutdown_function('saveCache');

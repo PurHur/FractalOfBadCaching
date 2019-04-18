d74079a52a4f952ae457253aa3ae7463801d256e
PurHur <purhur@purh.pw>
2019-04-18 18:17:42 +0200

    More documentation

--- cache_handling.php
+++ cache_handling.php
@@ -1,9 +1,14 @@
 <?php
+// load cache
 $GLOBALS['cache'] = @include('cache.php');
 
+// save cache
 function saveCache()
 {
+    // write cache file with loading code
     $cache = "<?php\nreturn unserialize('".serialize($GLOBALS['cache'])."');\n";
     file_put_contents('cache.php',$cache);
 }
+
+// auto save cache
 register_shutdown_function('saveCache');
